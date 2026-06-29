d22c0b99fbec6076c2c42ba324a69d58f84df3fc
Dario <dario.scazzosi@dondisalotti.it>
2026-06-29 11:22:13 +0200

    Revert "[FIX] spazi in embedded?"
    This reverts commit 98706967d10fe91a4670d52ecfb98ac5f990e920.

--- superset-frontend/src/embedded/index.tsx
+++ superset-frontend/src/embedded/index.tsx
@@ -21,15 +21,12 @@ import 'src/public-path';
 import { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Global } from '@emotion/react';
-import { t } from '@apache-superset/core/translation';
-import { makeApi } from '@superset-ui/core';
-import { logging } from '@apache-superset/core/utils';
 import {
   type SupersetThemeConfig,
-  ThemeMode,
-  css,
-} from '@apache-superset/core/theme';
+  makeApi,
+  t,
+  logging,
+} from '@superset-ui/core';
 import Switchboard from '@superset-ui/switchboard';
 import getBootstrapData, { applicationRoot } from 'src/utils/getBootstrapData';
 import setupClient from 'src/setup/setupClient';
@@ -93,16 +90,6 @@ const EmbededLazyDashboardPage = () => {
 
 const EmbeddedRoute = () => (
   <EmbeddedContextProviders>
-    <Global
-      styles={css`
-        /* Apply box-sizing reset for embedded dashboards to fix layout issues */
-        *,
-        *::before,
-        *::after {
-          box-sizing: border-box;
-        }
-      `}
-    />
     <Suspense fallback={<Loading />}>
       <ErrorBoundary>
         <EmbededLazyDashboardPage />
